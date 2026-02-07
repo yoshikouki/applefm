142804857a647d56adfb6c4fd469cfc9aec2b5e0
yoshikouki <yoshikouki@gmail.com>
2026-02-07 13:17:25 +0900

    refactor: restructure package into AppleFMCore library and applefm executable

--- Package.swift
+++ Package.swift
@@ -7,10 +7,31 @@ let package = Package(
     platforms: [
         .macOS(.v26),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.0"),
+    ],
     targets: [
+        .target(
+            name: "AppleFMCore",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ],
+            path: "Sources/AppleFMCore"
+        ),
         .executableTarget(
             name: "applefm",
-            path: "Sources"
+            dependencies: ["AppleFMCore"],
+            path: "Sources/applefm"
+        ),
+        .testTarget(
+            name: "AppleFMTests",
+            dependencies: ["AppleFMCore"],
+            path: "Tests/AppleFMTests"
+        ),
+        .testTarget(
+            name: "IntegrationTests",
+            dependencies: ["AppleFMCore"],
+            path: "Tests/IntegrationTests"
         ),
     ]
 )
---
+++ Sources/applefm/main.swift
@@ -0,0 +1,3 @@
+import AppleFMCore
+
+AppleFM.main()
